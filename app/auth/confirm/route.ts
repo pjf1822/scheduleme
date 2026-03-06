fd4794548ce510d73ecafe2b7464696e578dfb9f
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2026-03-06 12:25:58 -0500

    Update route.ts

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -25,6 +25,7 @@ export async function GET(request: NextRequest) {
       type,
       token_hash,
     });
+
     if (!error) {
       // redirect user to specified redirect URL or root of app
       redirect(next);
